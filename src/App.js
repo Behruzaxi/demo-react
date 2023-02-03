3862de9894c36c13dedb4d059159260204eda7d4
Behruzaxi <ebehruz846@gmail.com>
2023-02-03 15:29:23 +0500

    gfd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Button from './components/Button'
+import "./components/style.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [user, setUser] = useState("")
+const [message, setMessage] = useState(user)
+
+
+const onSubmitFunc = (e) => {
+setUser(e.target.value)
 }
+
+const handleClick = () => {
+setMessage(user)
+setUser("")
+
+}
+
+
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-6 mt-5">
+            <div className="mb-3">
+                <label for="exampleFormControlInput1" className="form-label">Email address</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="name@example.com"
+                    onChange={onSubmitFunc}
+                    value={!user.length !== 0 ? user : ""}
+                    />
+                    <button className='styl' onClick={handleClick}>Button</button>
+                    <h1>{message}</h1>
+                {/* <Button color="blue" name="Submit" /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./style.css"
+
+export default function Button(props) {
+  return (
+    <>
+    <button 
+    className={`${props.color} || red`}
+    // onClick={(e) => e.preventDefault() props.click || alert('error')}>
+    >
+        {props.name || "button"}
+    </button>
+    </>
+  )
+}
